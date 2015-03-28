6c0c8710d836433e88e54939dd5e1400f15f271b
mehedi4475 <mehedi4475@gmail.com>
2015-03-28 18:03:26 +0600

    SASS

---
+++ sass.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>SASS Tutorials</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<nav>
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Home</a></li>
+		</ul>
+	</nav>
+
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, fuga reprehenderit voluptates sunt, assumenda eius vitae ipsum recusandae consequuntur, facere eveniet perferendis earum quia pariatur modi asperiores impedit culpa dolores dignissimos repellendus. Itaque, ipsum sed sint magnam aperiam corporis ipsam.</p>
+	<button type="button" class="btn-a">Button A</button>
+	<button type="button" class="btn-b">Button B</button>
+	<button type="button" class="btn-c">Button C</button>
+	<button type="button" class="btn-d">Button D</button>
+	<button type="button" class="btn-e">Button E</button>
+	
+</body>
+</html>
